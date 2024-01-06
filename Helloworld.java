d3ef97846311e065d350d75c73d5fbb4f0efc270
kishordhangarsv <151608869+kishordhangarsv@users.noreply.github.com>
2024-01-07 01:18:42 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,5 @@
 // HelloWorld.java
-public class HelloWorld {
+public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins Pipeline!");
     }
